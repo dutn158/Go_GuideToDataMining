4f3bbaf6bae9e9df02a2092cedb0ec82c8027b51
Tran Du <untilyou158@gmail.com>
2016-07-29 08:12:38 +0700

    read scv

--- 2.RecommendationSystems/example2.go
+++ 2.RecommendationSystems/example2.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/csv"
+	"io"
+	"os"
+)
+
+type Film struct {
+	name   string
+	rating int64
+}
+
+type User struct {
+	name  string
+	films []Film
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var users = make([]User, 1)
+	count := 0
+	file, error := os.Open("../Movie_Ratings.csv")
+	if error != nil {
+		panic(error)
+	}
+	r := csv.NewReader(bufio.NewReader(file))
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		count2 := 0
+		if count == 0 {
+			count2++
+			user := User{name: record[count2]}
+			users = append(users, user)
+		}
+
+		if count > 0 {
+
+		}
+
+		count++
+	}
 }
